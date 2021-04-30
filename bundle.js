22a2ca3c00a1d04bd77fb60bdad5710d79654d62
zhoodar <ajoodar@gmail.com>
2021-04-30 15:46:44 +0600

    updated guest list

--- bundle.js
+++ bundle.js
@@ -5,7 +5,64 @@
             t.exports = e.p + "75371f53f06181df75f1.ttf"
         }, 296: function (t, r, e) {
             e(226);
-            var n = JSON.parse('{"9739472974927934792347":"Jumabai","9374957239452937459723":"Arthur"}');
+            var n = JSON.parse('{' +
+                '"B5niwjB6jUyxoNnZetKpA":"Мыскал",' +
+                '"W12t8Cl0eyHs6oeKARDA":"Арууке абла",' +
+                '"9lN5aHkCUUi83ulsH7FoOg":"Арина абла",' +
+                '"2szqMUxekqHrOe0k7B8Q":"Айгерим абла",' +
+                '"IkS2FtPBaEGmdRWXQjcpZA":"Жаңыл абла",' +
+                '"5M5aWJU4k0mTSK2Q0A99XA":"Назира абла",' +
+                '"4wtyj8YeaEizk0ZYMhAg":"Курманжан абла",' +
+                '"tdOkWxi38E6L5oRZKlZig":"Жылдыз абла",' +
+                '"FRe0ozGZlUv3gCrV56tg":"Зейнеп",' +
+                '"KlGsw5Dy4UqnXvSPDh9wrQ":"Рана",' +
+                '"ZIKTxmVxUaxIGNvQNOhcw":"Матлюба абла",' +
+                '"6TYLViKDDEWCGuQYNXDJbQ":"Сакура",' +
+                '"HpYJ0Dmog0SUHQUo22NMww":"Нуржамал",' +
+                '"cys9NbCWmUuUMqM4L0cqcw":"Уулкан эже",' +
+                '"gqwb8sGdqkSDteDgJYRi4w":"Аида эже",' +
+                '"ym5dZ2Pr0Wnyvb39eIjzg":"Бурмаш эже",' +
+                '"JuW3eIEwsUuu7E9IzhnkNQ":"Курманжан эже",' +
+                '"NvwUVBGIE6ZxgSSqOAtUg":"Айзирек эже",' +
+                '"symmtbvKWkivfGwuXiWRQ":"Бурмаш эже",' +
+                '"Gzmi9MRx9EWpXLeyfn61Fg":"Айпери эже",' +
+                '"OCwOWcZek2zMEoFlyBLvQ":"Пакиза эже",' +
+                '"Zb6xiBeU1ESfq1p9RRA":"Элина эже",' +
+                '"Poltxn1XkUmZD9YpH8K8bw":"Гулнур абла",' +
+                '"zPjTHFS7xkaQHTymZIcipQ":"Жаңыл",' +
+                '"Ra4dpgPpmUyNAAKC3RUMUA":"Айзада",' +
+                '"loDUIgiAwEKcHoPd98yxVg":"Алтынай",' +
+                '"teDeq45ek6disuaESYSQ":"Аселя",' +
+                '"5NwuUuvl5kCNjuTm3mytgQ":"Нора",' +
+                '"qLNzrrK1j0SD3Nlm4pOiAg":"Назбийке",' +
+                '"8lyiKIyE4UCMVGn7bdMcyg":"Перизат",' +
+                '"h86PqYS7kE66RZxAfMuEg":"Нуркыз",' +
+                '"iwB5a2fb1kWjvyFJg0sFKg":"Динара, Айчүрөк , Алтынай",' +
+                '"ozgJReWNsUghVHSMWgx1Q":"Салтанат",' +
+                '"5adPfExOESe58wq3aruQ":"Клара, Нуриза, Элнура, Жасмина, Айзирек, Симка, Кылым, Шамсия, Аселя Сүйүмкан, Айпери, Акылжан",' +
+                '"eiwKRS7TRkyUj9QsDSBbLQ":"Мээрим",' +
+                '"up8OqDIr5EqZ1Ia6HuJKQ":"Салима",' +
+                '"cGAH6tQm0egJk5log04tg":"Заркөкүл абла",' +
+                '"zyDQJVQVoUSPQMNfeS0Yjg":"Айнуска",' +
+                '"9K0NgeHY0KBeajnGG7Dg":"Перизат эже, Нурайым эже, Сезим эже",' +
+                '"BUzKxrBB4U2cfGGt6JH4vQ":"Таттыгүл",' +
+                '"0DZh2PE6cU6BP4qG7XtkaA":"Мээрим, Айка",' +
+                '"PHmWrfr90Uidsp8Z7LsLzw":"10 Б класс",' +
+                '"ycm4xSXckyiGDdUHqP1YQ":"Айшанура",' +
+                '"cc1vU9rTU2bEswHB3oRJw":"Гулкайыр",' +
+                '"5ENFeSNcLkS1OjAvg3mHOA":"Дилбара эже",' +
+                '"OfhHUQFGvkWMNDlHgXw1ew":"Жазгүл",' +
+                '"bMXdVotQa0mImlSYLB1Fw":"Эльвира",' +
+                '"t8Sf82Mbl0mQxJ0HD0P0rA":"Мираида",' +
+                '"IZfQECt3DUyU7A5xSXexPQ":"Зуура эже",' +
+                '"YtBVsqQxLkCPYDE6X64eQ":"Диана эже",' +
+                '"yw9nupMTAkKSmpg1NFBZSQ":"Бермет",' +
+                '"EBcXrxsZiEORS5l8ojKdLQ":"Сабина",' +
+                '"KYh9uN4L1kujVow38yJYHg":"Клара эже",' +
+                '"A6Crp0spqk6wmUklCKwOxQ":"Канзаада эже",' +
+                '"ZvZnhhOU3k2JhYUxgFIyBA":"Жыпара эже",' +
+                '"NMqUJLki306vK4BefPXLIg":"Айтурган",' +
+                '"lbQxlzLwWkq8x3tIxtsbDQ":"Айданек"}');
             document.getElementById("top").addEventListener("click", (function (t) {
                 t.preventDefault(), window.scroll({top: 0, left: 0, behavior: "smooth"})
             })), function (t) {
